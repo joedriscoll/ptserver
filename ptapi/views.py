7215e1f0e14af5fecb5154ec6bbef85fe41f1309
joedriscoll <josephdr@mit.edu>
2015-02-03 21:19:07 -0500

    changed get to post

--- ptapi/views.py
+++ ptapi/views.py
@@ -343,8 +343,8 @@ def getExercisesForPatient(request):
 def getPatientsExerciseData(request):
 #get the pair
 	print request
-	print request.GET['patient_username'] + 'hihi'
-	patient = User.objects.get(name = request.GET['patient_username'])
+	print request.POST['patient_username'] + 'hihi'
+	patient = User.objects.get(name = request.POST['patient_username'])
 	response = {"success":1, "all_exercises":[{"name":"hug","e_id":0, "e_sets":"there was a time", "e_assigned_days":[0,0,1,0,0,0,0]}], "current_exercises":[{"name":"kiss","e_id":0,"e_date":"8/11/14","e_completion":0}]}
 	response = getExerciseResponse(patient)
 	json_response = json.dumps(response)
